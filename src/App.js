e3a187938d52a0d2076cf3c407caec4b6d9bbaf9
Antonio Rodriguez <antoniwan@gmail.com>
2018-02-04 12:44:46 -0500

    init 🔥 create-react-app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">
+            monica rosado - digital account supervisor, digital marketing lover,
+            and social media expert living in miami, fl
+          </h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
