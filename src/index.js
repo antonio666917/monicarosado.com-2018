d9e5f193ad6bbc363954c9f661902ac7f3087a4c
Antonio Rodriguez <antoniwan@gmail.com>
2018-02-08 22:34:08 -0500

    Add basic Google Analytics tracking

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactGA from "react-ga";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
+ReactGA.initialize("UA-41380748-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
